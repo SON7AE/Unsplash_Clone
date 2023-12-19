596d07f6e459fb0fee38515e4f961933f5757dd9
SON7AE <bobsot0225@gmail.com>
2023-12-19 14:46:49 +0900

    페이지 라우팅 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,6 @@ import Home from "./pages/main/Home"
 function App() {
     return (
         <RecoilRoot>
-            {/* <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/edit" element={<Home />} />
-                <Route path="/following" element={<Home />} />
-                <Route path="/photoPlus" element={<Home />} />
-                <Route path="/oneColor" element={<Home />} />
-                <Route path="/3dRender" element={<Home />} />
-                <Route path="/nature" element={<Home />} />
-                <Route path="/texture" element={<Home />} />
-                <Route path="/interior" element={<Home />} />
-                <Route path="/film" element={<Home />} />
-                <Route path="/experimental" element={<Home />} />
-            </Routes> */}
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/:id" element={<Home />} />
